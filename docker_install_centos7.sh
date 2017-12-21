a09cb27999308a66707f0d2e23272b84ee30ce0e
mathcoder23 <mathcoder23@126.com>
2017-12-21 08:56:29 +0800

    修复自动确认问题

--- docker_install_centos7.sh
+++ docker_install_centos7.sh
@@ -1,19 +1,19 @@
 #安装docker
-yum install epel-release –y
+yum install -y epel-release 
 yum clean all
 yum list
-yum install docker-io –y
+yum install -y docker-io
 systemctl start docker
 docker info
 
 #docker 远程连接
 #set DOCKER_NETWORK_OPTIONS=" -H tcp://0.0.0.0:1314 -H unix:///var/run/docker.sock "
-sed  -i '/DOCKER_NETWORK_OPTIONS/c DOCKER_NETWORK_OPTIONS= -H tcp://0.0.0.0:1314 -H unix:///var/run/docker.sock '  /etc/sysconfig/docker-network 
-echo "更改docker network配置文件，配置远程ip与端口"
-cat /etc/sysconfig/docker-network   
-echo "更新docker service文件"
-systemctl daemon-reload
-echo "重启docker"
-systemctl restart docker
-echo "查看端口信息"
-netstat -ntpl
+#sed  -i '/DOCKER_NETWORK_OPTIONS/c DOCKER_NETWORK_OPTIONS= -H tcp://0.0.0.0:1314 -H unix:///var/run/docker.sock '  /etc/sysconfig/docker-network 
+#echo "更改docker network配置文件，配置远程ip与端口"
+#cat /etc/sysconfig/docker-network   
+#echo "更新docker service文件"
+#systemctl daemon-reload
+#echo "重启docker"
+#systemctl restart docker
+#echo "查看端口信息"
+#netstat -ntpl
