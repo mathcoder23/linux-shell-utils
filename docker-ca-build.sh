073cb198890d8cd6d8ab68e89ea1f576041db626
mathcoder23 <mathcoder23@126.com>
2018-01-29 09:53:35 +0800

    Update docker-ca-build.sh

--- docker-ca-build.sh
+++ docker-ca-build.sh
@@ -5,6 +5,8 @@ read dir
 echo "指定域名"
 HOST=api.zhongxin688.com
 read HOST
+echo "指定docker remote IP"
+read IP
 rm -rf $dir
 mkdir $dir
 echo "生成ca私钥 $dir/ca-key.pem"
@@ -15,14 +17,14 @@ echo "生成服务器私钥 $dir/server-key.pem"
 openssl genrsa -out $dir/server-key.pem 4096
 echo "生成证书请求文件 $dir/seerver.csr"
 openssl req -subj "/CN=$HOST" -sha256 -new -key $dir/server-key.pem -out $dir/server.csr
-echo subjectAltName = IP:172.17.0.1,IP:127.0.0.1,IP:172.31.21.206,IP:47.104.85.81 > $dir/extfile.cnf
+echo "subjectAltName = IP:172.17.0.1,IP:127.0.0.1,IP:172.31.21.206,IP:${IP}" > $dir/extfile.cnf
 echo "通过ca签署证书"
 openssl x509 -req -days 365 -sha256 -in $dir/server.csr -CA $dir/ca.pem -CAkey $dir/ca-key.pem \
-  -CAcreateserial -out $dir/server-cert.pem
+  -CAcreateserial -out $dir/server-cert.pem -extfile extfile.cnf
 echo "客户端证书 $dir/key.pem"
 openssl genrsa -out $dir/key.pem 4096
 echo "客户端请求文件 $dir/client.csr"
 openssl req -subj '/CN=client' -new -key $dir/key.pem -out $dir/client.csr
 echo "签署客户端文件"
 openssl x509 -req -days 365 -sha256 -in $dir/client.csr -CA $dir/ca.pem -CAkey $dir/ca-key.pem \
-  -CAcreateserial -out $dir/cert.pem 
+  -CAcreateserial -out $dir/cert.pem -extfile extfile.cnf
