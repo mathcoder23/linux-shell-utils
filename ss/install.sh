3e1e4a5be2b789e58426d9da5cd5abc48392fb7e
mathcoder23 <mathcoder23@126.com>
2018-02-15 17:01:58 +0800

    Update install.sh

--- ss/install.sh
+++ ss/install.sh
@@ -1,4 +1,4 @@
 #https://www.qcgzxw.cn/2039.html
-wget -N --no-check-certificate https://freed.ga/github/ssr-install.sh && bash ssr-install.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/mathcoder23/linux-shell-utils/master/ss/ssr-install.sh && bash ssr-install.sh
 
 wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/serverspeeder/master/serverspeeder-all.sh && bash serverspeeder-all.sh
