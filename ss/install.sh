7a0bd2729665399089a8b75c7cffbbcf7dbb03bc
mathcoder23 <mathcoder23@126.com>
2018-02-15 21:13:29 +0800

    Update install.sh

--- ss/install.sh
+++ ss/install.sh
@@ -1,4 +1,5 @@
 #https://www.qcgzxw.cn/2039.html
-wget -N --no-check-certificate https://raw.githubusercontent.com/mathcoder23/linux-shell-utils/master/ss/ssr-install.sh && bash ssr-install.sh
+#wget -N --no-check-certificate https://raw.githubusercontent.com/mathcoder23/linux-shell-utils/master/ss/ssr-install.sh && bash ssr-install.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssrmu.sh && chmod +x ssrmu.sh && bash ssrmu.sh
 wget -N --no-check-certificate https://freed.ga/kernel/ruisu.sh && bash ruisu.sh
 wget -N --no-check-certificate https://raw.githubusercontent.com/mathcoder23/linux-shell-utils/master/ss/serverspeeder-all.sh && bash serverspeeder-all.sh
