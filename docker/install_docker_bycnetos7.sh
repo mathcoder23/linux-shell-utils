d8429943cef7c0679d7786d75f8097cc77c5b6ef
mathcoder23 <mathcoder23@126.com>
2018-03-01 10:26:30 +0800

    Update install_docker_bycnetos7.sh

--- docker/install_docker_bycnetos7.sh
+++ docker/install_docker_bycnetos7.sh
@@ -1,5 +1,6 @@
 #安装docker
 prefix="#################"
+echo "${prefix}脚本更新于2018-03-01"
 config=`cat << EOF
 {
     "registry-mirrors": ["https://sbp3d98l.mirror.aliyuncs.com"]
@@ -8,15 +9,8 @@ EOF
 `
 
 
-function docker_compose_install(){
-    echo "${prefix}docker-compose安装"
-    yum -y install epel-release
-    yum -y install python-pip
-    pip install docker-compose
-    echo "${prefix}docker-compose安装完成,如果出现下面compose信息，说明安装成功"
-    docker-compose version
-}
 function docker_install(){
+    echo "${prefix}开始安装docker"
     yum install -y epel-release 
     yum clean all
     yum list
@@ -25,13 +19,23 @@ function docker_install(){
     echo "${prefix}docker安装完成,如果出现r下面docke信息，说明安装成功"
     docker info
 }
+function docker_compose_install(){
+    echo "${prefix}docker-compose安装"
+    yum -y install epel-release
+    yum -y install python-pip
+    pip install docker-compose
+    echo "${prefix}docker-compose安装完成,如果出现下面compose信息，说明安装成功"
+    docker-compose version
+}
 function docker_aliyun_speed(){
-    echo "${prefix}配置docker阿里云加速"
+    echo "${prefix}配置docker阿里云加速,这将覆盖/etc/docker/daemon.json，原始将会备份/etc/docker/daemon.json.bak"
     sudo mkdir -p /etc/docker
+    cp /etc/docker/daemon.json /etc/docker/daemon.json.bak
     sudo echo ${config} >> /etc/docker/daemon.json
 
     sudo systemctl daemon-reload
     sudo systemctl restart docker
 }
-
+docker_install
+docker_aliyun_speed
 docker_compose_install
