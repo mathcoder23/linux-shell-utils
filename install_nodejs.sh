3dc6a6f4ac416e68cdb2472ee423a60ca6cdb192
mathcoder23 <mathcoder23@126.com>
2018-03-16 17:48:20 +0800

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -2,5 +2,6 @@ sudo mkdir /usr/local/node
 cd /usr/local/node
 sudo wget  https://npm.taobao.org/mirrors/node/v8.9.3/node-v8.9.3-linux-x86.tar.gz
 tar -zxvf node-v8.9.3-linux-x86.tar.gz
-ln -s node-v8.9.3-linux-x86/bin/npm /usr/local/bin/ 
-ln -s node-v8.9.3-linux-x86/bin/node /usr/local/bin/ 
+ln -s /usr/loca/node/node-v8.9.3-linux-x86/bin/npm /usr/local/bin/ 
+ln -s /usr/loca/node/node-v8.9.3-linux-x86/bin/node /usr/local/bin/ 
+rm -rf /usr/local/node/node-v8.9.3-linux-x86.tar.gz
